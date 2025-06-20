830b23006f78e206f23a492737e31660dfa62152
Gr4ybyte <158534504+Gr4ybyte@users.noreply.github.com>
2025-06-20 01:58:28 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,68 @@
 // Contains all data about buildings and upgrades
 
-const buildings = [{'id': 'enchantedTower', 'name': 'Enchanted Tower', 'baseCost': 10, 'owned': 0, 'production': 1}, {'id': 'magicFamiliar', 'name': 'Magic Familiar', 'baseCost': 100, 'owned': 0, 'production': 5}, {'id': 'arcaneLibrary', 'name': 'Arcane Library', 'baseCost': 500, 'owned': 0, 'production': 20}, {'id': 'mysticPortal', 'name': 'Mystic Portal', 'baseCost': 2000, 'owned': 0, 'production': 75}, {'id': 'celestialObelisk', 'name': 'Celestial Obelisk', 'baseCost': 10000, 'owned': 0, 'production': 300}, {'id': 'runeSanctum', 'name': 'Rune Sanctum', 'baseCost': 50000, 'owned': 0, 'production': 1500}, {'id': 'manaWell', 'name': 'Mana Well', 'baseCost': 250000, 'owned': 0, 'production': 7000}, {'id': 'wizardTower', 'name': 'Wizard Tower', 'baseCost': 1000000, 'owned': 0, 'production': 30000}, {'id': 'phantasmGate', 'name': 'Phantasm Gate', 'baseCost': 5000000, 'owned': 0, 'production': 125000}, {'id': 'eternalSpire', 'name': 'Eternal Spire', 'baseCost': 20000000, 'owned': 0, 'production': 500000}];
+const buildings = [
+  { id: 'enchantedTower', name: 'Enchanted Tower', baseCost: 10, owned: 0, production: 1, baseProduction: 1 },
+  { id: 'magicFamiliar', name: 'Magic Familiar', baseCost: 100, owned: 0, production: 5, baseProduction: 5 },
+  { id: 'arcaneLibrary', name: 'Arcane Library', baseCost: 500, owned: 0, production: 20, baseProduction: 20 },
+  { id: 'mysticPortal', name: 'Mystic Portal', baseCost: 2000, owned: 0, production: 75, baseProduction: 75 },
+  { id: 'celestialObelisk', name: 'Celestial Obelisk', baseCost: 10000, owned: 0, production: 300, baseProduction: 300 },
+  { id: 'runeSanctum', name: 'Rune Sanctum', baseCost: 50000, owned: 0, production: 1500, baseProduction: 1500 },
+  { id: 'manaWell', name: 'Mana Well', baseCost: 250000, owned: 0, production: 7000, baseProduction: 7000 },
+  { id: 'wizardTower', name: 'Wizard Tower', baseCost: 1000000, owned: 0, production: 30000, baseProduction: 30000 },
+  { id: 'phantasmGate', name: 'Phantasm Gate', baseCost: 5000000, owned: 0, production: 125000, baseProduction: 125000 },
+  { id: 'eternalSpire', name: 'Eternal Spire', baseCost: 20000000, owned: 0, production: 500000, baseProduction: 500000 }
+];
 
-const upgrades = [{'id': 'clickPower', 'name': 'Focus Crystal', 'baseCost': 5, 'level': 0, 'maxLevel': 10, 'effectId': 'increaseClickPower'}, {'id': 'towerBoost', 'name': 'Tower Amplifier', 'baseCost': 10, 'level': 0, 'maxLevel': 3, 'effectId': 'boostTower_enchantedTower'}, {'id': 'magicFamiliarUpgrade1', 'name': 'Magic Familiar Upgrade 1', 'baseCost': 30, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_magicFamiliar'}, {'id': 'magicFamiliarUpgrade2', 'name': 'Magic Familiar Upgrade 2', 'baseCost': 40, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_magicFamiliar'}, {'id': 'magicFamiliarUpgrade3', 'name': 'Magic Familiar Upgrade 3', 'baseCost': 50, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_magicFamiliar'}, {'id': 'arcaneLibraryUpgrade1', 'name': 'Arcane Library Upgrade 1', 'baseCost': 110, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_arcaneLibrary'}, {'id': 'arcaneLibraryUpgrade2', 'name': 'Arcane Library Upgrade 2', 'baseCost': 120, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_arcaneLibrary'}, {'id': 'arcaneLibraryUpgrade3', 'name': 'Arcane Library Upgrade 3', 'baseCost': 130, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_arcaneLibrary'}, {'id': 'mysticPortalUpgrade1', 'name': 'Mystic Portal Upgrade 1', 'baseCost': 410, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_mysticPortal'}, {'id': 'mysticPortalUpgrade2', 'name': 'Mystic Portal Upgrade 2', 'baseCost': 420, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_mysticPortal'}, {'id': 'mysticPortalUpgrade3', 'name': 'Mystic Portal Upgrade 3', 'baseCost': 430, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_mysticPortal'}, {'id': 'celestialObeliskUpgrade1', 'name': 'Celestial Obelisk Upgrade 1', 'baseCost': 2010, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_celestialObelisk'}, {'id': 'celestialObeliskUpgrade2', 'name': 'Celestial Obelisk Upgrade 2', 'baseCost': 2020, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_celestialObelisk'}, {'id': 'celestialObeliskUpgrade3', 'name': 'Celestial Obelisk Upgrade 3', 'baseCost': 2030, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_celestialObelisk'}, {'id': 'runeSanctumUpgrade1', 'name': 'Rune Sanctum Upgrade 1', 'baseCost': 10010, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_runeSanctum'}, {'id': 'runeSanctumUpgrade2', 'name': 'Rune Sanctum Upgrade 2', 'baseCost': 10020, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_runeSanctum'}, {'id': 'runeSanctumUpgrade3', 'name': 'Rune Sanctum Upgrade 3', 'baseCost': 10030, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_runeSanctum'}, {'id': 'manaWellUpgrade1', 'name': 'Mana Well Upgrade 1', 'baseCost': 50010, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_manaWell'}, {'id': 'manaWellUpgrade2', 'name': 'Mana Well Upgrade 2', 'baseCost': 50020, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_manaWell'}, {'id': 'manaWellUpgrade3', 'name': 'Mana Well Upgrade 3', 'baseCost': 50030, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_manaWell'}, {'id': 'wizardTowerUpgrade1', 'name': 'Wizard Tower Upgrade 1', 'baseCost': 200010, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_wizardTower'}, {'id': 'wizardTowerUpgrade2', 'name': 'Wizard Tower Upgrade 2', 'baseCost': 200020, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_wizardTower'}, {'id': 'wizardTowerUpgrade3', 'name': 'Wizard Tower Upgrade 3', 'baseCost': 200030, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_wizardTower'}, {'id': 'phantasmGateUpgrade1', 'name': 'Phantasm Gate Upgrade 1', 'baseCost': 1000010, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_phantasmGate'}, {'id': 'phantasmGateUpgrade2', 'name': 'Phantasm Gate Upgrade 2', 'baseCost': 1000020, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_phantasmGate'}, {'id': 'phantasmGateUpgrade3', 'name': 'Phantasm Gate Upgrade 3', 'baseCost': 1000030, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_phantasmGate'}, {'id': 'eternalSpireUpgrade1', 'name': 'Eternal Spire Upgrade 1', 'baseCost': 4000010, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_eternalSpire'}, {'id': 'eternalSpireUpgrade2', 'name': 'Eternal Spire Upgrade 2', 'baseCost': 4000020, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_eternalSpire'}, {'id': 'eternalSpireUpgrade3', 'name': 'Eternal Spire Upgrade 3', 'baseCost': 4000030, 'level': 0, 'maxLevel': 1, 'effectId': 'boostTower_eternalSpire'}];
+const upgrades = [
+  { id: 'clickPower', name: 'Focus Crystal', baseCost: 5, level: 0, maxLevel: 10, effectId: 'increaseClickPower' },
+
+  // Enchanted Tower
+  { id: 'enchantedTowerUpgrade1', name: 'Sparkle Polish', baseCost: 10, level: 0, maxLevel: 1, effectId: 'boostTower_enchantedTower' },
+  { id: 'enchantedTowerUpgrade2', name: 'Mana Duct Tape', baseCost: 20, level: 0, maxLevel: 1, effectId: 'boostTower_enchantedTower' },
+  { id: 'enchantedTowerUpgrade3', name: 'Lightning Rod Hat', baseCost: 30, level: 0, maxLevel: 1, effectId: 'boostTower_enchantedTower' },
+
+  // Magic Familiar
+  { id: 'magicFamiliarUpgrade1', name: 'Snack Training', baseCost: 50, level: 0, maxLevel: 1, effectId: 'boostTower_magicFamiliar' },
+  { id: 'magicFamiliarUpgrade2', name: 'Mystic Leash', baseCost: 75, level: 0, maxLevel: 1, effectId: 'boostTower_magicFamiliar' },
+  { id: 'magicFamiliarUpgrade3', name: 'Fur Conditioner', baseCost: 100, level: 0, maxLevel: 1, effectId: 'boostTower_magicFamiliar' },
+
+  // Arcane Library
+  { id: 'arcaneLibraryUpgrade1', name: 'Librarian’s Coffee', baseCost: 125, level: 0, maxLevel: 1, effectId: 'boostTower_arcaneLibrary' },
+  { id: 'arcaneLibraryUpgrade2', name: 'Scroll Organizer', baseCost: 175, level: 0, maxLevel: 1, effectId: 'boostTower_arcaneLibrary' },
+  { id: 'arcaneLibraryUpgrade3', name: 'Book Levitation Charm', baseCost: 250, level: 0, maxLevel: 1, effectId: 'boostTower_arcaneLibrary' },
+
+  // Mystic Portal
+  { id: 'mysticPortalUpgrade1', name: 'Portal Polish', baseCost: 400, level: 0, maxLevel: 1, effectId: 'boostTower_mysticPortal' },
+  { id: 'mysticPortalUpgrade2', name: 'Void Navigator', baseCost: 600, level: 0, maxLevel: 1, effectId: 'boostTower_mysticPortal' },
+  { id: 'mysticPortalUpgrade3', name: 'Eldritch Air Freshener', baseCost: 900, level: 0, maxLevel: 1, effectId: 'boostTower_mysticPortal' },
+
+  // Celestial Obelisk
+  { id: 'celestialObeliskUpgrade1', name: 'Stargazer’s Lens', baseCost: 1200, level: 0, maxLevel: 1, effectId: 'boostTower_celestialObelisk' },
+  { id: 'celestialObeliskUpgrade2', name: 'Obelisk Tuning Fork', baseCost: 1500, level: 0, maxLevel: 1, effectId: 'boostTower_celestialObelisk' },
+  { id: 'celestialObeliskUpgrade3', name: 'Cosmic Wi-Fi', baseCost: 2000, level: 0, maxLevel: 1, effectId: 'boostTower_celestialObelisk' },
+
+  // Rune Sanctum
+  { id: 'runeSanctumUpgrade1', name: 'Rune Wax', baseCost: 3000, level: 0, maxLevel: 1, effectId: 'boostTower_runeSanctum' },
+  { id: 'runeSanctumUpgrade2', name: 'Sigil Recharger', baseCost: 5000, level: 0, maxLevel: 1, effectId: 'boostTower_runeSanctum' },
+  { id: 'runeSanctumUpgrade3', name: 'Parchment Blower', baseCost: 8000, level: 0, maxLevel: 1, effectId: 'boostTower_runeSanctum' },
+
+  // Mana Well
+  { id: 'manaWellUpgrade1', name: 'Bucket Brigade', baseCost: 10000, level: 0, maxLevel: 1, effectId: 'boostTower_manaWell' },
+  { id: 'manaWellUpgrade2', name: 'Well Whisperer', baseCost: 15000, level: 0, maxLevel: 1, effectId: 'boostTower_manaWell' },
+  { id: 'manaWellUpgrade3', name: 'Essence Chlorinator', baseCost: 20000, level: 0, maxLevel: 1, effectId: 'boostTower_manaWell' },
+
+  // Wizard Tower
+  { id: 'wizardTowerUpgrade1', name: 'Wizard HOA Membership', baseCost: 30000, level: 0, maxLevel: 1, effectId: 'boostTower_wizardTower' },
+  { id: 'wizardTowerUpgrade2', name: 'Cloud Elevator', baseCost: 40000, level: 0, maxLevel: 1, effectId: 'boostTower_wizardTower' },
+  { id: 'wizardTowerUpgrade3', name: 'Scrying Mirror WiFi', baseCost: 50000, level: 0, maxLevel: 1, effectId: 'boostTower_wizardTower' },
+
+  // Phantasm Gate
+  { id: 'phantasmGateUpgrade1', name: 'Echo Suppressant', baseCost: 75000, level: 0, maxLevel: 1, effectId: 'boostTower_phantasmGate' },
+  { id: 'phantasmGateUpgrade2', name: 'Illusion Optimizer', baseCost: 100000, level: 0, maxLevel: 1, effectId: 'boostTower_phantasmGate' },
+  { id: 'phantasmGateUpgrade3', name: 'Gate Grease', baseCost: 150000, level: 0, maxLevel: 1, effectId: 'boostTower_phantasmGate' },
+
+  // Eternal Spire
+  { id: 'eternalSpireUpgrade1', name: 'Clockstopper Charm', baseCost: 250000, level: 0, maxLevel: 1, effectId: 'boostTower_eternalSpire' },
+  { id: 'eternalSpireUpgrade2', name: 'Infinity Wax', baseCost: 400000, level: 0, maxLevel: 1, effectId: 'boostTower_eternalSpire' },
+  { id: 'eternalSpireUpgrade3', name: 'Endless Ladder', baseCost: 600000, level: 0, maxLevel: 1, effectId: 'boostTower_eternalSpire' }
+];
